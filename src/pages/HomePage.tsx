2819fa2562777e1a52fc07912891a575c5d7959e
IdrisVeli1987 <idrisveliev@gmail.com>
2025-03-28 17:47:08 +0400

    add dependencies

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>My MarketPlace</title>
+      </Helmet>
+      
+    </>
+  );
+};
+
+export default HomePage;
+
+
+// 1-22 minutes
